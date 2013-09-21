4c661a7139e1063dd0c64d53177b8ccbf9fe880a
Eli Perelman <eli@eliperelman.com>
2013-09-20 20:09:53 -0500

    Testing new reloading syntax

--- tasks/reload.js
+++ tasks/reload.js
@@ -4,12 +4,13 @@ var mark2 = require('../mark2');
 module.exports = function (grunt) {
 
     grunt.registerTask('reload-server', function (server) {
-        mark2.send(server, 'reload');
+        mark2.send(grunt.option('server') || server, 'reload');
     });
 
-    grunt.registerMultiTask('reload-', 'Reload all configured named servers', function () {
+    grunt.registerMultiTask('reload', 'Reload all configured named servers', function () {
         fn.each(function (server) {
-
+            grunt.option('server', server);
+            grunt.task.run('reload-server');
         }, this.data);
     });
 
